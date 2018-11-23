bc863096906d5a007399fbcb91f0f223be8b2179
AbhilashJN <abhilashjn107@gmail.com>
2018-11-23 13:03:22 +0530

    added sudo in shells cript

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install unzip
+sudo apt-get update && sudo apt-get install unzip
 export VANILLA_VERISON=2.6.4
 curl -o vanilla.zip "https://github.com/vanilla/vanilla/releases/download/Vanilla_${VANILLA_VERSION}/vanilla-${VANILLA_VERSION}.zip"
 unzip vanilla.zip -d ./app
