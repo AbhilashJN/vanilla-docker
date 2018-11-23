c966c45c3676ae3041657ad1f1acfd4be87ec63c
AbhilashJN <abhilashjn107@gmail.com>
2018-11-23 12:59:33 +0530

    changed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,3 @@ FROM php:7.2-apache
 ENV VANILLA_VERSION 2.6.4
 
 RUN docker-php-ext-install pdo pdo_mysql
-RUN apt-get update && apt-get install unzip
-RUN ls
-ADD "https://github.com/vanilla/vanilla/releases/download/Vanilla_${VANILLA_VERSION}/vanilla-${VANILLA_VERSION}.zip" /var/www/html/vanilla.zip
-RUN unzip /var/www/html/vanilla.zip
--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 apt-get update && apt-get install unzip
 export VANILLA_VERISON=2.6.4
-curl -o vanilla.zip "https://github.com/vanilla/vanilla/releases/download/Vanilla_${VANILLA_VERSION}/vanilla-${VANILLA_VERSION}.zip
+curl -o vanilla.zip "https://github.com/vanilla/vanilla/releases/download/Vanilla_${VANILLA_VERSION}/vanilla-${VANILLA_VERSION}.zip"
 unzip vanilla.zip -d ./app
 rm vanilla.zip
 sudo docker-compose up
